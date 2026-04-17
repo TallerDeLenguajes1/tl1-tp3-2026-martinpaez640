64cb5c0354a7cf67612d3e683f1aaae8ea096cee
martin paez <martinpaez640@gmail.com>
2026-04-16 21:35:27 -0300

    interfaz de usuario terminada

--- ejercicio_2/ArregloDeNombres.c
+++ ejercicio_2/ArregloDeNombres.c
@@ -6,17 +6,39 @@ void BuscaNombrePorId(char *p[], int num);
 int BuscaNombrePorPalabra(char *p[]);
 int main()
 {
+    int opcion = 0, id = 0;
     char *p[5], buff[50];
     for (int i = 0; i < 5; i++)
     {
-        printf("\nIngrese un numbre: ");
+        printf("\nIngrese un nombre: ");
         gets(buff);
         p[i] = (char *)malloc(sizeof(char) * (strlen(buff) + 1));
         strcpy(p[i], buff);
     }
+    printf("Para elegir buscar por nombre seleccione 1, para elegir buscar por coincidencia sleccione 2: ");
+    scanf("%d", &opcion);
+
+    if (opcion == 1)
+    {
+        printf("\nSeleccione un id");
+        scanf("%d", &id);
+        BuscaNombrePorId(p, 20);
+    }
+    else
+    {
+        getchar();
+        int f = BuscaNombrePorPalabra(p);
+        if (f != -1)
+        {
+            printf("\nEl nombre buscado por palabra es:  %s", p[f]);
+        }
+        else
+        {
+            printf("No se encontro coincidencias");
+        }
+    }
+
     MostrarPersonas(p);
-    BuscaNombrePorId(p, 20);
-    printf("El nombre buscado por palabra es:  %s", p[BuscaNombrePorPalabra(p)]);
 
     return 0;
 }
